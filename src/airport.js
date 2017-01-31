a5a909ba23235b617857a406d0c219cf9a72ac95
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-31 07:42:41 +0000

    Refactored

--- src/airport.js
+++ src/airport.js
@@ -6,21 +6,20 @@ function Airport(weather = new Weather, defaultCapacity = 20) {
 
 Airport.prototype.land = function(plane) {
   if(this._weather.isStormy()){throw new Error("cannot land plane: weather stormy");}
-  if(this._planes.length >= this._defaultCapacity){throw new Error("cannot land plane: airport is full");}
+  if(this.isFull()){throw new Error("cannot land plane: airport is full");}
   plane.land();
   this._planes.push(plane);
   return this._planes;
 };
 
 Airport.prototype.takeoff = function(plane) {
-  if (this._weather.isStormy()) {
-    throw new Error("cannot takeoff plane: weather stormy");
-  }
-  else
-  {
-    plane.takeoff();
-    var index = this._planes.indexOf(plane);
-    this._planes.splice(index, 1);
-    return this._planes;
-  }
+  if (this._weather.isStormy()){throw new Error("cannot takeoff plane: weather stormy");}
+  plane.takeoff();
+  var index = this._planes.indexOf(plane);
+  this._planes.splice(index, 1);
+  return this._planes;
+};
+
+Airport.prototype.isFull = function(){
+  if (this._planes.length >= this._defaultCapacity){return true}else{return false}
 };
